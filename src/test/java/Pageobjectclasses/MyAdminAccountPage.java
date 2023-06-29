cbfca763a1449c362e31eeccd425a0657f3a9f41
parkash212 <parkash212@gmail.com>
2023-06-29 18:03:17 +0530

    new 29 june commit

--- src/test/java/Pageobjectclasses/MyAdminAccountPage.java
+++ src/test/java/Pageobjectclasses/MyAdminAccountPage.java
@@ -11,20 +11,34 @@ public class MyAdminAccountPage extends BasePage {
 		super(driver);
 		
 }
+	//Evolving Love | Dashboard
+	// to find title of admin dashboard page 
+	@FindBy(xpath="//title[normalize-space()='Evolving Love | Dashboard']")
+	WebElement txt_msgHeading;
+	
 	@FindBy(xpath="//button[@id='userDrop']")
 	WebElement txt_userdrop;
-	
-	
 	
 	@FindBy(xpath="//a[normalize-space()='Log Out']")
 	WebElement txt_logout;
 	
-	public void logout() {
-		js.executeScript("arrguments[0].click();",txt_userdrop);
+	public boolean isMyAdminAccountPageExists() {
+		try {
+			return (msgHeading.isDisplayed());
+		} catch (Exception e) {
+			return (false);
+		}
 	}
+	
 	public void userdrop() {
 		js.executeScript("arrguments[0].click();",txt_logout);
 	}
+	
+	public void logout() {
+		js.executeScript("arrguments[0].click();",txt_userdrop);
+	}
+	
+	
 	
 	
 }
